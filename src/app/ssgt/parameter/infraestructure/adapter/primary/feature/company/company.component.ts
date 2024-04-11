9c3218198ed345794ef3a178a5a745eda6f918a9
felipeanfepe <felipeanfepe@gmail.com>
2024-04-11 16:55:33 -0500

    feat: create component general

--- src/app/ssgt/parameter/infraestructure/adapter/primary/feature/company/company.component.ts
+++ src/app/ssgt/parameter/infraestructure/adapter/primary/feature/company/company.component.ts
@@ -1,33 +1,87 @@
 import { CommonModule } from '@angular/common'
-import { Component, inject } from '@angular/core'
-// import { ListService } from 'src/app/ssgt/core/application/list.service';
-import { AppBreadcrumbService } from '../../root/breadcrumb/app.breadcrumb.service'
+import { Component } from '@angular/core'
+import { ButtonsGeneralComponent } from '../../../../../../../shared/components/buttons-general/buttons-general.component';
+import { TableGeneralComponent } from '../../../../../../../shared/components/table-general/table-general.component';
+import { AppBreadcrumbService } from '../../root/breadcrumb/app.breadcrumb.service';
+import { Column } from '../../../../../../../shared/components/table-general/col/col';
+
 
 @Component({
     selector: 'app-company',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, ButtonsGeneralComponent, TableGeneralComponent],
     templateUrl: './company.component.html',
 })
 export class CompanyComponent {
-
-    // private service = inject(ListService);
-    // products$ = this.service.getLists();
-
-    constructor(
-        private breadcrumbService: AppBreadcrumbService,
-        
-    ) {
+    constructor(private breadcrumbService: AppBreadcrumbService) {
         this.breadcrumbService.setItems([
             { label: 'Home', routerLink: ['/'] },
             { label: 'Empresas', routerLink: ['/administration/company'] },
         ])
     }
 
-    
-    // getList() {
-    //     this.service.getLists().subscribe(res => {
-    //         var list = res;
-    //     });
-    // }
+
+    public retornaColumnas(): Array<Column> {
+        return [
+            {
+                title: 'Codigo',
+                data: 'codigo',
+                sort: 'codigo',
+                width: '10%'
+            },
+            {
+                title: 'Nombre',
+                data: 'nombre',
+                sort: 'nombre',
+                width: '20%'
+            },
+            {
+                title: 'Descripcion',
+                data: 'descripcion',
+                sort: 'descripcion',
+                width: '40%',
+            },
+            {
+                title: 'Favorito',
+                data: 'favorito',
+                sort: 'favorito',
+                width: '10%',
+                classTitle: 'text-center',
+                classCode: 'text-center',
+                classStatus: 'text-center',
+                render: (favorito: any) => {
+                    if (favorito == 1) {
+                        return '<i class="pi pi-star text-red-500"></i>'
+                    }
+                return
+                }
+            },
+            {
+                title: 'Activo',
+                data: 'activo',
+                sort: 'activo',
+                width: '10%',
+                classTitle: 'text-center',
+                classCode: 'text-center',
+                classStatus: 'text-center',
+                render: (activo: any) => {
+                    if (activo == 1) {
+                        return '<i class="pi pi-check-circle text-green-500"></i>'
+                    }
+                    if (activo == 2) {
+                        return '<i class="pi pi-times-circle text-red-500"></i>'
+                    }
+                    return
+                }
+            },
+            {
+                title: 'Acciones',
+                data: '',
+                sort: '',
+                width: '10%',
+                classTitle: 'text-center no-pointer',
+                actions: ['btn_eliminar', 'btn_editar'],
+            },
+        ];
+    }
 }
