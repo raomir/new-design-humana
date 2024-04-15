394c105b5b867fd79ef1a93cf30bb59c21d25d3e
JohanyDev <johany1996@hotmail.com>
2024-04-15 10:51:55 -0500

    Fix: Tarea #53967 ajuste v2

--- src/app/shared/components/tree-table-general/tree-table-general.component.ts
+++ src/app/shared/components/tree-table-general/tree-table-general.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { TreeTableModule } from 'primeng/treetable';
@@ -11,6 +11,7 @@ import { ButtonsGeneralComponent } from '../buttons-general/buttons-general.comp
   imports: [CommonModule, TreeTableModule, ButtonModule, ButtonsGeneralComponent],
   templateUrl: './tree-table-general.component.html',
   styleUrls: ['./tree-table-general.component.css'],
+  encapsulation: ViewEncapsulation.None,
   styles: [
   ]
 })
