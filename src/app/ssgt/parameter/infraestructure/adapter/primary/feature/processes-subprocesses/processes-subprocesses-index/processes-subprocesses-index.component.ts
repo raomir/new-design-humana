aae86bbf62dfae8c74c1313578646f390d064967
JohanyDev <johany1996@hotmail.com>
2024-04-29 08:34:49 -0500

    Feat: Historia de Usuario #53819 v1

---
+++ src/app/ssgt/parameter/infraestructure/adapter/primary/feature/processes-subprocesses/processes-subprocesses-index/processes-subprocesses-index.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-processes-subprocesses-index',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './processes-subprocesses-index.component.html',
+  styles: [
+  ]
+})
+export class ProcessesSubprocessesIndexComponent {
+
+}
